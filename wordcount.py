80c2ab29e55ea78878689d95d42877b14c7e481a
Huy Ngo <huy.ngo@codelink.io>
2018-12-11 17:07:56 +0700

    Add `hashtag` sample

--- wordcount.py
+++ wordcount.py
@@ -8,12 +8,12 @@ import apache_beam as beam
 import re
 
 with beam.Pipeline() as p:
-    lines = ( p | beam.io.ReadFromText('data/paragraph.txt')
-                | 'Split_Words' >> beam.FlatMap(lambda x: re.findall(r'[A-Za-z\']+', x))
-                | 'Map_To_One' >> beam.Map(lambda x: (x, 1))
-                | 'Combine_And_Count' >> beam.CombinePerKey(sum)        # Combine And Count equals to 2 steps below
-                # | 'Combine' >> beam.GroupByKey()
-                # | 'Count' >> beam.Map(lambda x: (x[0], sum(x[1])))
-                | 'Format' >> beam.Map(lambda x: '{}: {}'.format(*x))
-                | 'Write' >> beam.io.WriteToText('output/worldcount.txt')
+    lines = ( p | 'read' >> beam.io.ReadFromText('data/paragraph.txt')
+                | 'split_worlds' >> beam.FlatMap(lambda x: re.findall(r'[A-Za-z\']+', x))
+                | 'map_to_one' >> beam.Map(lambda x: (x, 1))
+                | 'combine_and_count' >> beam.CombinePerKey(sum)        # Combine And Count equals to 2 steps below
+                # | 'combine' >> beam.GroupByKey()
+                # | 'count' >> beam.Map(lambda x: (x[0], sum(x[1])))
+                | 'format' >> beam.Map(lambda (word, count): '{}: {}'.format(word, count))
+                | 'write' >> beam.io.WriteToText('output/worldcount.txt')
                 )
